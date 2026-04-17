308ed08d97066837922945b4c1214f6e5d496b6c
Pedro Henrique Chaves Junqueira <pedrojunqueira.ufg@gmail.com>
2026-04-17 08:19:01 -0300

    Configurando cronometro

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import BarraLateral from './components/BarraLateral.vue';
+import Formulario from './components/Formulario.vue';
+import { defineComponent } from 'vue';
+
+defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral, Formulario
+  }
+});
+</script>
+
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+
+    </div>
+
+    <div class="column is-three-quarters">
+      <Formulario />
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
